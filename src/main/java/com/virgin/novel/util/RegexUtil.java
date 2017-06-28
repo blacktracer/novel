31e50d586a4feefa6d3fe753c1e1e91de5d98f82
zhengjiudong <zhengjiudong@palmyou.com>
2017-06-28 11:07:51 +0800

    测试 git stash

--- src/main/java/com/virgin/novel/util/RegexUtil.java
+++ src/main/java/com/virgin/novel/util/RegexUtil.java
@@ -3,5 +3,6 @@ package com.virgin.novel.util;
 public class RegexUtil {
 	public static void main(String[] args) {
 		System.out.println("hello,git branch merge conflict");
+		System.out.println("admin");
 	}
 }
