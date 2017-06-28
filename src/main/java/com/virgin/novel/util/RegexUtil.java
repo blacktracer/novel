8236711f7ce6ad1f6fcb8e34b19a6b802b31334e
zhengjiudong <zhengjiudong@palmyou.com>
2017-06-28 11:33:56 +0800

    开发分支

--- src/main/java/com/virgin/novel/util/RegexUtil.java
+++ src/main/java/com/virgin/novel/util/RegexUtil.java
@@ -2,8 +2,6 @@ package com.virgin.novel.util;
 
 public class RegexUtil {
 	public static void main(String[] args) {
-		System.out.println("hello,git branch merge conflict");
-		System.out.println("admin");
-		System.out.println("hi");
+		System.out.println("hello,git branch");
 	}
 }
