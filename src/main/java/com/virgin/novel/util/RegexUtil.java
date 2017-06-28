e86cd391b494c6071784ecaea717f95cdf41a8fa
zhengjiudong <zhengjiudong@palmyou.com>
2017-06-28 11:14:12 +0800

    解决git stash pop冲突

--- src/main/java/com/virgin/novel/util/RegexUtil.java
+++ src/main/java/com/virgin/novel/util/RegexUtil.java
@@ -4,5 +4,6 @@ public class RegexUtil {
 	public static void main(String[] args) {
 		System.out.println("hello,git branch merge conflict");
 		System.out.println("admin");
+		System.out.println("hi");
 	}
 }
